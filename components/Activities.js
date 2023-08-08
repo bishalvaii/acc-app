bdf76ed07e2daf3389d9dd66c3e4e61ed30cb2ae
Vishal Adhikari <vishaladhikari1738@gmail.com>
2023-08-08 16:06:23 +0545

    added quiz functionality

--- components/Activities.js
+++ components/Activities.js
@@ -1,20 +1,14 @@
-// imageUrls: [
-//     '../assets/swing.jpg',
-//     '../assets/swing1.jpg'
-// ]
-
-// },
-// {
-
-// category: "Cycling",
-// imageUrls: [
-//    '../assets/cycling.jpg',
-//    '../assets/cycling1.jpg'
-// ]
-import React from 'react';
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, ScrollView, TouchableOpacity, Button } from 'react-native';
+import { db } from '../firebase';
+import { useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore/lite';
 
 const ActivitiesScreen = () => {
+  const [usersData, setUsersData] = useState([]);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+
   const images = [
     {
       category: 'Swing',
@@ -32,8 +26,90 @@ const ActivitiesScreen = () => {
     },
   ];
 
+  useEffect(() => {
+    const fetchUsersData = async () => {
+      try {
+        const answersCollection = collection(db, 'answers'); // Replace with your collection name
+        const answersSnapshot = await getDocs(answersCollection);
+
+        const userDataArray = [];
+        answersSnapshot.forEach((doc) => {
+          userDataArray.push({ userId: doc.data().userID, questions: doc.data().questions, answers: doc.data().answers });
+        });
+
+        setUsersData(userDataArray);
+        console.log(userDataArray);
+      } catch (error) {
+        console.error('Error fetching users data:', error);
+      }
+    };
+
+    fetchUsersData();
+  }, []);
+  
+
+  const handleSubmit = async () => {
+    if (selectedAnswerIndex !== null) {
+      try {
+        setIsSubmitting(true);
+
+        // Send the selected answer to the Firebase database
+        // You can use the selectedAnswerIndex to get the selected answer from usersData
+        // For example: const selectedAnswer = usersData[userIndex].answers[selectedAnswerIndex];
+        
+        // Perform your submission to Firebase here
+        // ...
+
+        setIsSubmitting(false);
+        setSelectedAnswerIndex(null); // Clear the selected answer after submission
+      } catch (error) {
+        console.error('Error submitting answer:', error);
+        setIsSubmitting(false);
+      }
+    }
+  };
+
+ 
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title1}>Play Quiz</Text>
+      <Text style={styles.subTopic}>Answer the correct one and get a chance to get a free ticket to the cable car</Text>
+      {usersData.map((userData, userIndex) => (
+        <View key={userIndex} style={styles.userContainer}>
+          <Text style={styles.question}>Question: {userData.questions}</Text>
+          <Text style={styles.userAnswers}>Select an answer:</Text>
+          <View style={styles.answerOptions}>
+          {userData.answers.map((answer, answerIndex) => (
+              <TouchableOpacity
+                key={answerIndex}
+                style={[
+                  styles.option,
+                  selectedAnswerIndex === answerIndex ? styles.selectedOption : null,
+                ]}
+                disabled={userData.selectedAnswer !== undefined}
+                onPress={() => setSelectedAnswerIndex(answerIndex)} // Update selected answer index
+              >
+                <Text style={styles.optionText}>{answer}</Text>
+                {selectedAnswerIndex === answerIndex && (
+                  <Image
+                    source={require('../assets/checkmark.png')} // Replace with your green checkmark image
+                    style={styles.checkmark}
+                  />
+                )}
+              </TouchableOpacity>
+            ))}
+            
+            
+          </View>
+        </View>
+      ))}
+       <Button
+        title={isSubmitting ? 'Submitting...' : 'Submit Answer'}
+        onPress={handleSubmit}
+        disabled={isSubmitting || selectedAnswerIndex === null} // Disable if no answer selected
+      />
+      {/* ... Rest of the JSX ... */}
       <Text style={styles.title}>Activities to Do</Text>
       {images.map((category, index) => (
         <View key={index}>
@@ -45,7 +121,7 @@ const ActivitiesScreen = () => {
           </ScrollView>
         </View>
       ))}
-    </View>
+    </ScrollView>
   );
 };
 
@@ -54,6 +130,29 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: '#FFFFFF',
+  },
+  title1: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subTopic: {
+    fontSize: 16,
+    marginBottom: 15,
+  },
+  userContainer: {
+    marginBottom: 20,
+  },
+  question: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  userAnswers: {
+    fontSize: 16,
+  },
+  answerOptions: {
+    flexDirection: 'column',
   },
   title: {
     fontSize: 24,
@@ -65,11 +164,33 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 5,
   },
+
   image: {
     width: 150,
     height: 150,
     borderRadius: 10,
     marginRight: 10,
+  },
+  
+  selectedOption: {
+    backgroundColor: '#EFEFEF',
+  },
+  optionText: {
+    fontSize: 16,
+  },
+  option: {
+    flexDirection: 'row', // Aligns the option and checkmark horizontally
+    justifyContent: 'space-between', // Places the checkmark at the rightmost part
+    alignItems: 'center', // Vertically centers the content
+    borderWidth: 1,
+    borderColor: '#333',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  checkmark: {
+    width: 20,
+    height: 20,
   },
 });
 
